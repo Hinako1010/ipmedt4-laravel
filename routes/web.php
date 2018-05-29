38b48f4f92a03b1577ec4aed2a4d1d6545ac20a4
Unknown <s1103171@student.hsleiden.nl>
2018-05-29 20:45:43 +0200

    halve diaryentrycontroller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/newentry', 'DiaryEntryController@create');
+Route::post('/newentry', 'DiaryEntryController@store');
