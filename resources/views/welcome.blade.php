676303cd19ac2cd298b2ca889bbb5f01809abad2
MelissaJ <mels1999@live.nl>
2018-06-03 10:40:15 +0200

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,6 +71,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ url('/home') }}">Login</a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                 
