d3419d4ce0df7dcd85f48790c6ca5c4c84c5cb3c
MelissaJ <mels1999@live.nl>
2018-06-03 10:42:08 +0200

    link naarhome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,6 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ url('/home') }}">Login</a>
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
                 
