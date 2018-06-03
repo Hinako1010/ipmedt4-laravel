40a9a20fbfd7155c63078310f5d932e5136d558a
MelissaJ <mels1999@live.nl>
2018-06-03 10:33:32 +0200

    backcolor

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #9febf4;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
