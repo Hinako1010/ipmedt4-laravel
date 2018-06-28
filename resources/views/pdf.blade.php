6fb2487da328cb84e87655c2a099d5ec100d48b1
Dion van den Berg <dionberg@gmail.com>
2018-06-28 14:34:02 +0200

    tabel fatsoelijk uitgeleind voor de data

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -13,10 +13,23 @@
         }
         #grafieken{
             margin-bottom: 8px;
+
         }
-        #tabelletje table, th, td{
-            /*padding: 10px;*/
-            /*border: 1px solid black;*/
+        #tabelletje1 {
+            padding-top: 30px;
+
+        }
+        #symptomenlijst td:nth-child(2), th:nth-child(2)  {
+            padding-left: 50px;
+
+        }
+        #symptomenlijst td:nth-child(3) ,th:nth-child(3) {
+            padding-left: 50px;
+
+        }
+
+        #tabelletje td{
+            margin-left: 50px;
         }
 
     </style>
@@ -39,7 +52,7 @@
     De inhoud/content van Medisch dagboek is niet bedoeld als vervanging van professioneel medisch advies, diagnose en/of behandeling. Raadpleeg altijd uw huisarts of een andere bevoegde zorgverlener, als u onzeker bent over (de gevolgen van) een medische aandoening.
     </td>
   </table>
-<table id="tabelletje" class="table table-bordered">
+<table id="symptomenlijst" class="table table-bordered">
 
     <tr>
         <th><br><br><u>Symptomen</u></th>
@@ -49,43 +62,52 @@
     <tr>
         <td>Onverklaarbare koude rillingen</td>
         <td>3 dagen</td>
-        <td></td>
+        <td>28-6-2018 - 1-7-2018</td>
     </tr>
     <tr>
         <td>Pijnlijke keel</td>
         <td>1 dag</td>
+        <td>26-6-2018</td>
     </tr>
     <tr>
         <td>Maagklachten</td>
         <td>3 dagen</td>
+        <td>23-6-2018 - 26-6-2018</td>
     </tr>
     <tr>
         <td>Hartkloppingen</td>
         <td>1 dag</td>
+        <td>1-7-2018</td>
     </tr>
     <tr>
         <td>Pijn in de borstkas</td>
         <td>1 dag</td>
+        <td>1-7-2018</td>
     </tr>
     <tr>
         <td>Hoofdpijn</td>
         <td>5 dagen</td>
+        <td>20-6-2018 - 25-6-2018</td>
     </tr>
     <tr>
         <td>Pijn of jeuk in oren</td>
         <td>1 dag</td>
+        <td>29-6-2018</td>
     </tr>
     <tr>
-        <td>Pijn in de onderbuik<br><br><br></td>
-        <td>3 dagen<br><br><br></td>
+        <td>Pijn in de onderbuik</td>
+        <td>3 dagen</td>
+        <td>29-6-2018 - 2-7-2018</td>
     </tr>
 </table>
-<table class="table table-bordered">
+
+<table id="tabelletje1" class="table table-bordered" >
   <tr id="grafieken">
       <td>
           <img src="https://i.imgur.com/GEZWWFo.png" height="200" width="350">
       </td>
-      <td><img src="https://i.imgur.com/fmczBhu.png" height="200" width="350"></td>
+      <td>
+          <img src="https://i.imgur.com/fmczBhu.png" height="200" width="350"></td>
   </tr>
 
 
